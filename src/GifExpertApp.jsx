11e3236c62135cf4cf951ae08f927cc8858db25e
Erick Andrade <erick_andradhe@hotmail.com>
2023-02-13 19:58:23 -0600

    feat(app):development of components

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,30 +1,41 @@
 import { useState } from "react"
+import { AddCategory } from "./components/AddCategories";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState([ 'One Punch Man', 'Naruto']);
+    const [categories, setCategories] = useState([ 'One Punch Man' ]);
     // const [categories, setCategories] = useState('Naruto');
 
-    const onAddCategory = () => {
+    const onAddCategory = ( newCategory ) => {
         // setCategories(categories.concat('halo'));//una opcion para aumentar el arreglo
-        setCategories([ ...categories, 'Halo' ]);//otra opcion para agregar al arreglo
+        if( categories.includes(newCategory) ) return;
+        setCategories([ ...categories, newCategory ]);//otra opcion para agregar al arreglo
         // setCategories(categories.push('halo'));//no se puede hacer asi
+        
+        // categories.push( newCategory );
+        // console.log(categories);
 
     }
 
     return(
         <>
+            {/* titulo */}
             <h1>GifExpertApp</h1>
 
-            <button onClick={onAddCategory}>Agregar</button>
+            {/* Input */}
+            < AddCategory 
+                onNewCategory = { ( value ) => onAddCategory( value ) }
+            />
 
-            <ol>
-                {
-                    categories.map( category => {
-                        return <li key={ category }> { category } </li>
-                    })
-                }
-            </ol>
+            {
+                categories.map( category => (
+                    <GifGrid 
+                        key={ category }
+                        category={ category }
+                    />
+                ))
+            }   
         </>
     )
 }
