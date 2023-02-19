7565719301fbdb7dec1c18fccd350dcf28c7d63a
Erick Andrade <erick_andradhe@hotmail.com>
2023-02-18 21:21:15 -0600

    feat(gifGrid):creation of specific component to handle the gifs and add is loading

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,26 +1,27 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect( () => {
-        getGifs( category )
-            .then( newImages => setImages(newImages) );
-    }, [])
-
+    const { images, isLoading } = useFetchGifs( category );
+    console.log(isLoading)
     return (
         <>
             <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
 
-            <ol>
+            <div className="card-grid">
                 {
-                    images.map( ({id, title}) => (
-                        <li key={ id }> { title } </li>
+                    images.map( image => (
+                        <GifItem 
+                            key={ image.id }
+                            { ...image }
+                        />
                     ))
                 }
-            </ol>
+            </div>
         </>
     )
 }
